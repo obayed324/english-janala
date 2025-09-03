e0480cd5ed70756bac8e1fe33741f4e67258c093
Mohammad Obayed Sarker <127292015+obayed324@users.noreply.github.com>
2025-09-03 23:08:36 +0600

    load lessons and word by API

--- script/index.js
+++ script/index.js
@@ -6,6 +6,47 @@ const loadLessons = () =>{
     .then(json => displayLessons(json.data))
 }
 
+const loadLevelWord =(id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(json => displayLevelWord((json.data)))
+} 
+
+// {
+//     "id": 5,
+//     "level": 1,
+//     "word": "Eager",
+//     "meaning": "আগ্রহী",
+//     "pronunciation": "ইগার"
+// }
+
+const displayLevelWord = (words) => {
+    const  wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        console.log(word);
+
+        const card = document.createElement("div");
+        card.innerHTML = ` <div class=" bg-white rounded-xl shadow-sm text-center py-15 px-5 space-y-4">
+        <h2 class=" font-bold text-xl">${word.word}</h2>
+        <p class=" font-semibold">Meaning /Pronounciation</p>
+
+        <div class=" text-2xl font-medium font-bangla">"${word.meaning}"</div>
+        <div class=" flex justify-between items-center">
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            
+        </div>
+
+    </div>`
+
+        wordContainer.append(card);
+    });
+}
+
 const displayLessons = (lessons) => {
     // 1.get the container & empty
     const levelContainer = document.getElementById("level-container");
@@ -17,7 +58,7 @@ const displayLessons = (lessons) => {
         console.log(lesson);
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button type="button" class="btn btn-outline btn-primary">
+        <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
         <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
         </button>
         `;
